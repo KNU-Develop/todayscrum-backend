7d91008b7dae18901f3299637143248517dc4da5
gunhee <rjsgml771@naver.com>
2024-08-29 15:17:12 +0900

    fix/api: api 연결과정에서 발생한 사소한 문제 수정

--- src/main/java/knu/kproject/entity/user/User.java
+++ src/main/java/knu/kproject/entity/user/User.java
@@ -90,22 +90,22 @@ public class User {
     private String color;
 
     public User(String name, String socialId, String email, UserStatus status) {
-        this.name = name;
-        this.socialId = socialId;
-        this.email = email;
+        this.name = name.trim();
+        this.socialId = socialId.trim();
+        this.email = email.trim();
         this.status = status;
     }
 
     public void updateUserInfo(UpdateUserDto userInfo) {
         if (userInfo.getName() != null && !userInfo.getName().trim().isEmpty()) {
-            this.name = userInfo.getName();
+            this.name = userInfo.getName().trim();
         }
         if (userInfo.getContact() != null && !userInfo.getContact().trim().isEmpty()) {
             this.contact = userInfo.getContact();
         }
         this.marketingEmailOptIn = userInfo.isMarketingEmailOptIn();
         this.mbti = userInfo.getMbti();
-        this.location = userInfo.getLocation();
+        this.location = userInfo.getLocation().trim();
         this.imageUrl = userInfo.getImageUrl();
     }
 
@@ -116,7 +116,7 @@ public class User {
         if (userInfo.getContact() == null || userInfo.getContact().trim().isEmpty()) {
             throw new IllegalArgumentException("Contact cannot be empty.");
         }
-        this.name = userInfo.getName();
+        this.name = userInfo.getName().trim();
         this.contact = userInfo.getContact();
         this.status = UserStatus.JOIN;
         this.marketingEmailOptIn = userInfo.isMarketingEmailOptIn();
@@ -125,13 +125,14 @@ public class User {
 
     public void addAdditionalInfo(AdditionalUserInfo userInfo) {
         this.mbti = userInfo.getMbti();
-        this.location = userInfo.getLocation();
+        this.location = userInfo.getLocation().trim();
         this.imageUrl = userInfo.getImageUrl();
     }
 
     public void withDraw(UserDto userDto) {
         this.status = UserStatus.WITHDRAW;
     }
+
     /*
         User <-> Schedule 연관 관계 메서드
      */
