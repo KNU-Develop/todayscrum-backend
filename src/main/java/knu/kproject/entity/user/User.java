46b88b44d5071c4ea84e7956574a4cd7fdf0455d
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2024-09-23 15:43:03 +0900

    feat : User color 추가

--- src/main/java/knu/kproject/entity/user/User.java
+++ src/main/java/knu/kproject/entity/user/User.java
@@ -89,7 +89,7 @@ public class User {
     @Column
     private String color;
 
-    public User(String name, String socialId, String email, UserStatus status, String color) {
+    public User(String name, String socialId, String email, UserStatus status) {
         this.name = name.trim();
         this.socialId = socialId.trim();
         this.email = email.trim();
